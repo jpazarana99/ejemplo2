d65e02b035f8ebdbbf5cd22361c63b5539ae6149
jpazarana99 <jpazarana99@gmail.com>
2021-01-06 18:35:03 -0600

    suma en h1

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const  mult = (a,b) => a * b ;
 const div = (a,b) => a / b;
 
 document.addEventListener("DOMContentLoaded",() => {
+
 	console.log("Archivo cargado");
+	const content = document.getElementById("mensaje");
+	content.innerHTML = suma(10,10);
 });
