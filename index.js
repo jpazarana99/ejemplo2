23522b1836e3a474ded0ab478472bfe695c12843
jpazarana99 <jpazarana99@gmail.com>
2021-01-06 18:31:53 -0600

    Empezando con la cargada de las funciones

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const suma = (a,b)=> a+b;
 
-const  mult = (a,b,c,d) => a * b * c *d;
+const  mult = (a,b) => a * b ;
 
+const div = (a,b) => a / b;
 
-console.log(suma(10,5));
-console.log(mult(suma(5,10),1,2));
+document.addEventListener("DOMContentLoaded",() => {
+	console.log("Archivo cargado");
+});
